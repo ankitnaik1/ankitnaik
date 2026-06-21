ea99f0bde88710a6647d2f8439202709429002ee
Ankit <ankitnaik16@gmail.com>
2026-06-21 16:03:09 +0530

    Update InstallAdobe.ps1

--- static/InstallAdobe.ps1
+++ static/InstallAdobe.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 
 Write-Host "Downloading Adobe Acrobat Reader..." -ForegroundColor Cyan
-Invoke-WebRequest -Uri "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400220687/AcroRdrDCx642400220687_en_US.exe" -OutFile "$env:TEMP\AcroRdrDCx64_en_US.exe"
+Invoke-WebRequest -Uri "https://ardownload3.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2600121662/AcroRdrDCx642600121662_MUI.exe" -OutFile "$env:TEMP\AcroRdrDCx64_en_US.exe"
 
 # Run the Installer Silently
 # /sAll = Silent mode for all, /msi = pass parameters to MSI, /norestart = suppress reboot
